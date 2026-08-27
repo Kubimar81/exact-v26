ff9f9a50bb3e231a6c3afbe43800c2315e9f60f7
Kubimar81 <kubimar81@wp.pl>
2026-08-27 16:22:34 +0200

    Dodaj EPL board

---
+++ src/components/analysis/epl-board.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+
+export function EplBoard() {
+  return (
+    <Card className="p-5">
+      <h3 className="font-display text-xl">Ranking EPL</h3>
+    </Card>
+  );
+}
