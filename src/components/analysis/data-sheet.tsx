6c338f1f31e05fd0f3069888f8f444829efe43d1
Kubimar81 <kubimar81@wp.pl>
2026-08-27 16:23:57 +0200

    Dodaj arkusz danych

---
+++ src/components/analysis/data-sheet.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+
+export function DataSheet() {
+  return (
+    <Card className="p-5">
+      <h3 className="font-display text-xl">Arkusz danych</h3>
+    </Card>
+  );
+}
