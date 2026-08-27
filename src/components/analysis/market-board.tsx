7ea141d4ddc691f2c85dc0840539572243f6eb82
Kubimar81 <kubimar81@wp.pl>
2026-08-27 16:22:00 +0200

    Dodaj MarketBoard

---
+++ src/components/analysis/market-board.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+
+export function MarketBoard() {
+  return (
+    <Card className="p-5">
+      <h3 className="font-display text-xl">Top 3 zakłady</h3>
+    </Card>
+  );
+}
